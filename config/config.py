497113665edf1d04d7318a4d45d1ff56b58ffc94
sachinopboy2 <sachin25u7@gmail.com>
2025-07-17 17:10:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -76,10 +76,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/Professor_bot_updates"
+    "SUPPORT_CHANNEL", "https://t.me/NOBITA_BOTS"
 )  # Example:- https://t.me/akaChampu
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/Professor_bot_updates"
+    "SUPPORT_GROUP", "https://t.me/NOBITA_BOTS"
 )  # Example:- https://t.me/TheChampuClub
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", True)
@@ -101,7 +101,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", 5))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/shizukaofficial/shizukaxchampuoffoceal")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/sachinopboy2/Champunew")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
@@ -171,7 +171,7 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://telegra.ph/file/d3e855bc548a1ce9649e7.jpg",
+    "https://graph.org/file/c6d3d07bbfa468765ae50-d934e643e622400213.jpg",
 )
 
 PING_IMG_URL = getenv(
